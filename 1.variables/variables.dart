cae3f3f77579aaf2949a3d8bd649ff058a523309
unknown <pablo.herrera.celis@unillanos.edu.co>
2022-09-03 09:49:18 -0500

    Avance clase 9:50am

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -1,1 +1,13 @@
-void main() {}
+void main() {
+  var nombre = "jaime";
+  String nombreDos = "jaime";
+
+  final String nombreTres = "jaime";
+  // nombreTres = "jaime";
+  const String nombreCuatro = "jaime";
+  //nombreCuatro = "jaime andres";
+
+  print(nombre);
+  print(nombreDos);
+}
+
--- 2.tipos/tipos.dart
+++ 2.tipos/tipos.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  int variable1 = 23;
+  double variable2 = 1.8;
+  var variable3 = 23;
+  var variable4 = 1.8;
+
+  print(variable1);
+  print(variable2);
+  print(variable2.ceil());
+  print(variable2.floor());
+  print(variable2.abs());
+}
